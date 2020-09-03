b66f36b3d548e045a0e894b539a22abed6a7edc0
STORYM <ssashkaa01@gmail.com>
2020-09-03 12:51:11 +0300

    add hw

---
+++ 03.09.2020/Web.Shop/Controllers/NewsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.Shop.Entities;
+using Web.Shop.Models;
+using Web.Shop.Repo.Interafaces;
+using Web.Shop.Services.Interfaces;
+
+namespace Web.Shop.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly ILogger<NewsController> _logger;
+        private readonly INewsService _newsService;
+
+        public NewsController(ILogger<NewsController> logger,
+            INewsService newsService)
+        {
+            _logger = logger;
+            _newsService = newsService;
+        }
+
+        public async Task<IActionResult> Index(NewsFilterVM filter, int page=1)
+        {
+            var usersCount = HttpContext.Session.GetString("username");
+            var model = _newsService.GetNews(filter, page);
+            return View(model);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(NewsAddVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                _newsService.AddNews(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+
+    }
+}
---
+++ 03.09.2020/Web.Shop/Repo/Implement/CategoryRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Shop.Entities;
+using Web.Shop.Repo.Interafaces;
+
+namespace Web.Shop.Repo.Implement
+{
+    public class CategoryRepo : BaseRepository<Category, long>, ICategoryRepo
+    {
+        public CategoryRepo(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
