27894b5fbfcb64442ada1e8b6a5cac7fca2a8ef2
STORYM <ssashkaa01@gmail.com>
2020-09-11 23:17:26 +0300

    add hw

---
+++ 11.09.2020/Web.Shop/Models/CategoryVM.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Shop.Entities;
+
+namespace Web.Shop.Models
+{
+
+    public class CategoryVM
+    {
+        public int Page { get; set; }
+        public int maxPage { get; set; }
+        public List<Category> list { get; set; }
+        public CategoryFilterVM CategoryFilter { get; set; }
+    }
+    public class CategoryAddVM
+    {
+        [Display(Name = "Назва категорії")]
+        [Required(ErrorMessage ="Вкажіть назву")]
+        public string Name { get; set; }
+
+        [Display(Name = "URL Slug")]
+        [Required(ErrorMessage = "Вкажіть slug")]
+        public string UrlSlug { get; set; }
+
+        [Display(Name = "Фото")]
+        [Required(ErrorMessage = "Оберіть фото")]
+        public IFormFile Image { get; set; }
+    }
+
+    public class CategoryEditVM
+    {
+        [Display(Name = "Назва категорії")]
+        [Required(ErrorMessage = "Вкажіть назву")]
+        public string Name { get; set; }
+
+        [Display(Name = "URL Slug")]
+        [Required(ErrorMessage = "Вкажіть slug")]
+        public string UrlSlug { get; set; }
+
+        [Display(Name = "Вибрати нове фото")]
+        public IFormFile ImageUpload { get; set; }
+
+        [Display(Name = "Фото")]
+        public string Image { get; set; }
+    }
+
+    public class CategoryFilterVM
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Назва категорії")]
+        public string Name { get; set; }
+
+    }
+}
